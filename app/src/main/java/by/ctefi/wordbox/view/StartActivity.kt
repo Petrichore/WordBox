d7cec0d9fb0f9cfde6941f54983f856121e42972
Petrichore <jetrichor@gmail.com>
2019-10-03 17:19:03 +0300

    init commit: 1) some libraries are configured 2) start activity is created

---
+++ app/src/main/java/by/ctefi/wordbox/view/StartActivity.kt
@@ -0,0 +1,13 @@
+package by.ctefi.wordbox.view
+
+import android.app.Activity
+import android.os.Bundle
+import by.ctefi.wordbox.R
+
+class StartActivity: Activity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
